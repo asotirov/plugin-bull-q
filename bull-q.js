fffbf27cf00015429e9d460f41f0ddaefae042c0
Anton Sotirov <anton.k.sotirov@gmail.com>
2018-08-19 19:44:34 +0300

    Initializing Q's as Singletons

--- bull-q.js
+++ bull-q.js
@@ -1,22 +1,29 @@
 'use strict';
 const Queue = require('bull');
-
+const _qs = Symbol('queues');
 module.exports = ['q', ({
-                            options: {name: oName, redis: oRedis} = {}
+                            redis: pRedis
                         }) => {
-    return {
-        q: {
-            /**
-             * Creates a Bull.Queue
-             * @param name
-             * @param redis
-             * @return {Bull | Bull.Queue | * | Queue}
-             */
-            Q: (name = oName, redis = oRedis) => {
-                return new Queue(name, typeof redis === 'string' ? redis : {
-                    redis
-                });
+    const q = {
+        [_qs]: {},
+        initQ: (name = 'DEFAULT', {redis = pRedis, limiter, prefix, defaultJobOptions, settings} = {}) => {
+            q[_qs][name] = new Queue(name, {redis: redis.options, limiter, prefix, defaultJobOptions, settings});
+            return q.Q(name);
+        },
+        /**
+         * Returns the queue with the specified name if it is initialized
+         * @param name
+         * @return {Bull | Bull.Queue | * | Queue}
+         */
+        Q: (name) => {
+            const qToReturn = q[_qs][name];
+            if (!qToReturn) {
+                throw new Error(`${name} bull q not initialized. Call q.initQ(name...) to initialize.`);
             }
+            return qToReturn;
         }
-    }
+    };
+    return {
+        q
+    };
 }];
